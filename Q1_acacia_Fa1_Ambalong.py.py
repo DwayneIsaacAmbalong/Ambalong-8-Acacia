b3ef957c0c66ce866c2ddec5cb14a99f6c7a3197
DwayneIsaacAmbalong <dibambalong31@src.pshs.edu.ph>
2026-08-11 09:49:57 +0800

    Enhance code readability with comments
    Added comments to clarify the steps in calculating the distance between two points.

--- Q1_acacia_Fa1_Ambalong.py.py
+++ Q1_acacia_Fa1_Ambalong.py.py
@@ -1,19 +1,25 @@
 import math
 
-x1 = int(input("What is the x1? "))
-x2 = int(input("What is the x2? "))
-y1 = int(input("What is the y1? "))
-y2 = int(input("What is the y2? "))
+x1 = int ( input ( "What is the x1? " ) )
+x2 = int ( input ( "What is the x2? " ) )
+y1 = int ( input ( "What is the y1? " ) )
+y2 = int ( input ( "What is the y2? " ) )
+#The codes on top of this comment are the questions that will ask the inputs your are going to put
 
 x3 = x2 - x1
 y3 = y2 - y1
+#This is the first step in solving the distance of the 2 points
 
-x = pow(x3,2)
-y = pow(y3,2)
+x = pow (x3 , 2 )
+y = pow (y3 , 2 )
+#This is the 2nd step in solving the distance of the 2 points
 
 d1 = x + y
-d = (math.sqrt(d1))
-print("The distance between the two points is: ",d, )
+d = ( math.sqrt ( d1 ) )
+#this is the final step
+
+print ( "The distance between the two points is: " ,d, )
+#this code will print the output
 
 #Reflection
 # I learned how to use libraries and other function and also I learned proper variable placement or else it will be wrong.
